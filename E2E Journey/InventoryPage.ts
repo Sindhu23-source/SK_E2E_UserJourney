3c216156e56d37377978cc3aa261c7ddcdab137c
Sindhu23-source <69655755+Sindhu23-source@users.noreply.github.com>
2026-02-08 22:14:33 +1300

    Refactor InventoryPage class methods and properties

--- E2E Journey/InventoryPage.ts
+++ E2E Journey/InventoryPage.ts
@@ -1,19 +1,25 @@
-import { Page, Locator } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test';
 
 export class InventoryPage {
-  private readonly backButton: Locator;
-  private readonly productNames: Locator;
+  private page: Page;
+  private backToProductsBtn: Locator;
+  private productList: Locator;
 
-  constructor(private readonly page: Page) {
-    this.backButton = page.locator('#back-to-products');
-    this.productNames = page.locator('.inventory_item_name');
+  constructor(page: Page) {
+    this.page = page;
+    this.backToProductsBtn = page.locator('#back-to-products');
+    this.productList = page.locator('.inventory_item_name');
   }
 
-  async openProduct(index: number) {
-    await this.productNames.nth(index).click();
+  async openProductByIndex(index: number) {
+    await expect(this.productList).toHaveCountGreaterThan(index);
+
+    await this.productList.nth(index).click();
   }
 
-  async goBackToProducts() {
-    await this.backButton.click();
+  async returnToInventory() {
+    if (await this.backToProductsBtn.isVisible()) {
+      await this.backToProductsBtn.click();
+    }
   }
 }
